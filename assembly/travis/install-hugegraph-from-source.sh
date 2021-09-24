5ad065e3cac3bf90b8702e84e78b9727a0813279
guoyonggang <guoyonggang@baidu.com>
2021-09-24 10:11:04 +0800

    prepare env git checkout

--- assembly/travis/install-hugegraph-from-source.sh
+++ assembly/travis/install-hugegraph-from-source.sh
@@ -8,7 +8,7 @@ if [[ $# -ne 1 ]]; then
 fi
 
 BRANCH_ID=$1
-HUGEGRAPH_GIT_URL="https://github.com/starhugegraph/hugegraph.git"
+HUGEGRAPH_GIT_URL="git@github.com:starhugegraph/hugegraph.git"
 
 git clone --depth 100 ${HUGEGRAPH_GIT_URL}
 cd hugegraph
